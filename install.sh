fa00e8405ed5fdf194f0f5d5198ad5b275823f8b
Thomas Lange <57691168+quadraserver@users.noreply.github.com>
2023-12-13 15:07:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 o#!/bin/bash
 clear
 echo ""
+echo "--------------------------------------------------------"
+echo ""
 echo "         Welcome to the:"
 echo ""
-echo "  __          __   _ _                               _____           _        _ _"           
-echo "  \ \        / /  | | |                             |_   _|         | |      | | |"          
-echo "   \ \  /\  / /_ _| | |_ __   __ _ _ __   ___ _ __    | |  _ __  ___| |_ __ _| | | ___ _ __" 
-echo "    \ \/  \/ / _` | | | '_ \ / _` | '_ \ / _ \ '__|   | | | '_ \/ __| __/ _` | | |/ _ \ '__|"
-echo "     \  /\  / (_| | | | |_) | (_| | |_) |  __/ |     _| |_| | | \__ \ || (_| | | |  __/ |"   
-echo "      \/  \/ \__,_|_|_| .__/ \__,_| .__/ \___|_|    |_____|_| |_|___/\__\__,_|_|_|\___|_|"   
-echo "                      | |         | |"                                                       
-echo "                      |_|         |_|"                                                       
+echo "            Wallpaper Installer"                             
 echo ""
-echo "                                     2023 by Thomas Lange"
+echo "                    2023 by Thomas Lange"
 echo ""
+echo "--------------------------------------------------------"
 echo ""
 git clone https://www.github.com/quadraserver/wallpaper.git
 cd wallpaper
